0284e508b7255cd7c8d2c4add1bbad7098cef09e
Ayokitan Olowofoyeku <91305910+ayokitanfoyeh@users.noreply.github.com>
2023-09-10 12:58:41 -0400

    change

--- logapp.py
+++ logapp.py
@@ -3,12 +3,11 @@ import pandas as pd
 import random
 from datetime import datetime
 
-# Define the events and possible values with more descriptive names
+# Define the events and possible values
 events_values = {
     'Insulin Basal Rate': {'unit': 'Units/Hour', 'min': 0.5, 'max': 2.0},
     'Blood Glucose Level': {'unit': 'mg/dL', 'min': 70, 'max': 180},
     'Insulin Bolus Dose': {'unit': 'Units', 'min': 1, 'max': 10},
-    'Carb Intake': {'unit': 'Grams', 'min': 0, 'max': 100},
     'Battery Level': {'unit': '%', 'min': 0, 'max': 100}
 }
 
@@ -40,33 +39,50 @@ for timestamp in timestamps:
 
 df = pd.DataFrame(entries)
 
+# Set the index name to "Log ID"
+df.index.name = "Log ID"
+
 # Create the app
 st.title("Insulin Pump Log Entries")
 
-# Add 'All' option to the events list
-all_events = ['All'] + list(events_values.keys())
+# Add radio button for Log ID filter
+log_id_filter = st.radio("Filter by Log ID:", ('All Logs', 'Specific Log ID'))
 
-# Filter by event
-selected_event = st.selectbox("Filter by Event", all_events)
-
-if selected_event == 'All':
-    filtered_df = df.copy()
+# If 'Specific Log ID' is selected, show number input for Log ID
+if log_id_filter == 'Specific Log ID':
+    specific_log_id = st.number_input("Enter Log ID", min_value=0, max_value=len(df) - 1, value=0)
+    filtered_df_by_id = df[df.index == specific_log_id]
+    
+    if filtered_df_by_id.empty:
+        st.write("No entries found for the specified Log ID.")
+    else:
+        st.write("Log Entries:")
+        st.dataframe(filtered_df_by_id)
 else:
-    filtered_df = df[df['Event'] == selected_event]
+    # Add 'All' option to the events list
+    all_events = ['All'] + list(events_values.keys())
 
-# Get the earliest and latest dates from the DataFrame
-earliest_date = df['Timestamp'].min().date()
-latest_date = df['Timestamp'].max().date()
+    # Filter by event
+    selected_event = st.selectbox("Filter by Event", all_events)
 
-# Filter by date
-start_date = st.date_input("Filter by Start Date", value=earliest_date)
-end_date = st.date_input("Filter by End Date", value=latest_date)
+    if selected_event == 'All':
+        filtered_df = df.copy()
+    else:
+        filtered_df = df[df['Event'] == selected_event]
 
-start_date = pd.Timestamp(datetime.combine(start_date, datetime.min.time()))
-end_date = pd.Timestamp(datetime.combine(end_date, datetime.max.time()))
+    # Get the earliest and latest dates from the DataFrame
+    earliest_date = df['Timestamp'].min().date()
+    latest_date = df['Timestamp'].max().date()
 
-filtered_df = filtered_df[(filtered_df['Timestamp'] >= start_date) & (filtered_df['Timestamp'] <= end_date)]
+    # Filter by date
+    start_date = st.date_input("Filter by Start Date", value=earliest_date)
+    end_date = st.date_input("Filter by End Date", value=latest_date)
 
-# Display the filtered DataFrame
-st.write("Log Entries:")
-st.dataframe(filtered_df)
+    start_date = pd.Timestamp(datetime.combine(start_date, datetime.min.time()))
+    end_date = pd.Timestamp(datetime.combine(end_date, datetime.max.time()))
+
+    filtered_df = filtered_df[(filtered_df['Timestamp'] >= start_date) & (filtered_df['Timestamp'] <= end_date)]
+
+    # Display the filtered DataFrame
+    st.write("Log Entries:")
+    st.dataframe(filtered_df)

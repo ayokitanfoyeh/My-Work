2a2a73bc3eb0c8cb3fe6e0d797e77e374c4ed4b8
Ayokitan Olowofoyeku <91305910+ayokitanfoyeh@users.noreply.github.com>
2023-09-10 10:49:33 -0400

    Low battery default value & fixed mismatch rows

--- logapp.py
+++ logapp.py
@@ -4,8 +4,13 @@ import random
 from datetime import datetime
 
 # Define the events and possible values
-events = ['Basal Rate', 'Blood Glucose', 'Bolus', 'Carbohydrates', 'Low Battery']
-values = ['1.0 Units/Hour', '120 mg/dL', '4 Units', '30 Grams']
+events_values = {
+    'Basal Rate': {'unit': 'Units/Hour', 'min': 0.5, 'max': 2.0},
+    'Blood Glucose': {'unit': 'mg/dL', 'min': 70, 'max': 180},
+    'Bolus': {'unit': 'Units', 'min': 1, 'max': 10},
+    'Carbohydrates': {'unit': 'Grams', 'min': 0, 'max': 100},
+    'Low Battery': {'unit': '%', 'min': 0, 'max': 100}
+}
 
 # Generate 1000 rows of entries
 timestamps = pd.date_range(start='2023-09-01', periods=1000, freq='15min')
@@ -17,14 +22,20 @@ for timestamp in timestamps:
     elif random.random() < 0.05:
         event = 'Bolus'
     else:
-        event = random.choice(events)
+        event = random.choice(list(events_values.keys()))
     
-    value = random.choice(values)
-    value_parts = value.split(' ')
-    current_value = float(value_parts[0])
-    new_value = random.uniform(current_value - 5, current_value + 5)
+    unit = events_values[event]['unit']
+    
+    if event == 'Low Battery':
+        new_value = 0.1
+    else:
+        min_value = events_values[event]['min']
+        max_value = events_values[event]['max']
+        new_value = random.uniform(min_value, max_value)
+    
     new_value = round(new_value, 2)
-    entry = {'Timestamp': timestamp, 'Event': event, 'Value': new_value, 'Units': value_parts[1]}
+    
+    entry = {'Timestamp': timestamp, 'Event': event, 'Value': new_value, 'Units': unit}
     entries.append(entry)
 
 df = pd.DataFrame(entries)
@@ -33,7 +44,7 @@ df = pd.DataFrame(entries)
 st.title("Insulin Pump Log Entries")
 
 # Add 'All' option to the events list
-all_events = ['All'] + events
+all_events = ['All'] + list(events_values.keys())
 
 # Filter by event
 selected_event = st.selectbox("Filter by Event", all_events)
